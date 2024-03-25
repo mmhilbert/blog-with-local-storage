2abf738603b191058c3f81f76c810735d103eede
Megan Hilbert <meganmhilbert@gmail.com>
2024-03-25 14:51:15 -0500

    completed JavaScript functionality to retrieve blog list from local storage and build HTML content.

--- assets/js/blog.js
+++ assets/js/blog.js
@@ -1,3 +1,7 @@
+const blogListSection = document.getElementById("blog-list")
+blogListSection.innerHTML = ''
+
+
 const getPostsFromLS = function() {
     const blogArrayString = localStorage.getItem("blogArray")
 
@@ -11,7 +15,28 @@ const getPostsFromLS = function() {
         create blog post element for each post in the loop
         add to blog-list section in the loop
     */
+   for (const blogPost of blogArray) {
+    console.log(blogPost)
+    addBlogPostToBlogList(blogPost)
+   }
 }
 
+const addBlogPostToBlogList = function(blogPostObject) {
+    let blogListDiv = document.createElement("div")
+    blogListDiv.classList.add('blog-post')
+    const h3 = document.createElement('h3')
+    const pContent = document.createElement('p')
+    const pUsername = document.createElement('p')
+
+    h3.innerText = `Title: ${blogPostObject.title}`
+    pContent.innerText = `Content: ${blogPostObject.content}`
+    pUsername.innerText = `Posted By: ${blogPostObject.username}`
+
+    blogListDiv.appendChild(h3)
+    blogListDiv.appendChild(pContent)
+    blogListDiv.appendChild(pUsername)
+
+    blogListSection.appendChild(blogListDiv)
+}
 
 getPostsFromLS()
