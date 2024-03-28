450473352526d98997348eb4013a31344e4de9e9
Megan Hilbert <meganmhilbert@gmail.com>
2024-03-28 14:21:22 -0500

    updated darkmode css, used local storage to store darkmode, updated button css.

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -5,4 +5,20 @@ const darkmodeToggle = document.getElementById("darkmode-toggle")
 darkmodeToggle.addEventListener('change', function() {
     let body = document.querySelector('body')
     body.classList.toggle('darkmode')
+
+    let isDarkMode = body.classList.contains("darkmode")
+    localStorage.setItem('isDarkMode', isDarkMode)
+
 })
+
+let initDarkMode = function() {
+    let isDarkMode = localStorage.getItem('isDarkMode')
+    console.log(isDarkMode)
+    if (isDarkMode == "true") {
+        let body = document.querySelector('body')
+        body.classList.add('darkmode')
+        darkmodeToggle.setAttribute('checked', true)
+    }
+}
+
+initDarkMode()
